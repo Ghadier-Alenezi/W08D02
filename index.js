2ca6d1d2f5db3409cbae0421eeeb1ed21a34d721
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-11-29 16:45:02 +0300

    back-end initial setup contain main packages express, nodemon, dotenv, cors, bcrypt and  jsonwebtoken

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () =>{
+    console.log(`SERVER ON ${PORT}`);
+});
