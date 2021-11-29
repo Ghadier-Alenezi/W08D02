33024fbe7786e0f365a4d5438c980bcabeea1eed
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-11-29 20:50:11 +0300

    create my first endpoints, add new role to the db the second is get all the roles from the db, start the code in the main index.js test the code it works fine! add admin and user roles with thier premissions to the db

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const roleModel = require("./db/models/role"); 
+app.post("/newRole", (req, res)=>{
+    const {role , Permissions}  = req.body;
+    const newRole = new roleModel({
+        role , Permissions
+    })
+    newRole.save().then((result)=>{
+        res.json(result);
+    })
+    .catch((err)=>{
+        res.send(err);
+    })
+
+})
+
+app.get("/roles", (req, res)=>{
+    roleModel.find({}).then((result)=>{
+        res.send(result)
+    }).catch((err)=>{
+        res.send(err);
+
+    })
+})
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
